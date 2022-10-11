680fec6553c793c7f04a81d41662cb192faff19f
Ruteryan <felichan98@gmail.com>
2022-10-11 08:57:15 +0200

    first commit

---
+++ bridge.py
@@ -0,0 +1,13 @@
+import paho.mqtt.client as mqtt
+
+class Bridgino:
+
+    def __init__(self):
+        # parameters init
+        self._boh = []
+
+    def something(self, param):
+        pass
+    
+
+
