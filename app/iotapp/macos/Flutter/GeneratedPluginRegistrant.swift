a72b0314cfe8635eca752232cb77f9696cd92e5a
Ruteryan <felichan98@gmail.com>
2023-02-01 15:16:51 +0100

    app pubspec.lock

--- app/iotapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/iotapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-=======
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
->>>>>>> 211ed61d3cb06cf4b567dbe2af230f1e4f4796fd
 }
--- app/iotapp/windows/flutter/generated_plugin_registrant.cc
+++ app/iotapp/windows/flutter/generated_plugin_registrant.cc
@@ -6,14 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-<<<<<<< HEAD
 #include <geolocator_windows/geolocator_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   GeolocatorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("GeolocatorWindows"));
-=======
-
-void RegisterPlugins(flutter::PluginRegistry* registry) {
->>>>>>> 211ed61d3cb06cf4b567dbe2af230f1e4f4796fd
 }
